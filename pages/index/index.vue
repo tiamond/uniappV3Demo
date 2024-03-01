31e44bf3f4b2533321ff2a1b047f006cd65ece1d
liuqi <723432427@qq.com>
2024-03-01 20:34:54 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<button class="btn-def" hover-class="btn-active" type="default" @click="routeTo">demo1</button>
+</template>
+
+<script setup>
+	const routeTo = () => {
+		uni.navigateTo({
+			url: '/pages/demo1/demo1'
+		})
+	}
+</script>
+
+<style lang="scss" scoped>
+	.btn-def {
+		width: 200upx;
+		margin: 0;
+	}
+	.btn-active {
+		background: red;
+	}
+</style>
