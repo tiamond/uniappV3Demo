663ba412009420ad9da0117ab854a1063511fa81
liuqi <723432427@qq.com>
2024-03-03 21:43:46 +0800

    demo: component，withDefaults,defineProps,defineEmit,slot

---
+++ components/Child/Child.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<view v-for="(item,index) in list" :key="index">
+			<slot :data="item"></slot>
+		</view>
+		<button type="default" @click="onClick">触发事件</button>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	const emit = defineEmits<{
+		(event : 'add', id : number) : void
+	}>()
+
+	interface item {
+		name : string
+	}
+	const props = withDefaults(defineProps<{
+		list : item[]
+	}>(), ({
+		list: () => [{ name: 'default' }]
+	}))
+	const onClick = () => {
+		emit('add', Math.random())
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/demo5/demo5.vue
@@ -0,0 +1,28 @@
+<template>
+	<Child @add = 'onAdd' :list = 'list'>
+		<template #default="{data}">
+			<view>{{data.name}}</view>
+		</template>
+	</Child>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from 'vue'
+	const list = ref<{name: string}[]>([{
+		name: '123'
+	}, 
+	{
+		name: '123'
+	}, {
+		name: '123'
+	}])
+const onAdd = (num: number) => {
+	list.value.push({
+		name: String(num)
+	})
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
