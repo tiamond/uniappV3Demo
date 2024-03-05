d199d3ce2924d71cfd7ff2d30b89a8eaac2465f3
liuqi <723432427@qq.com>
2024-03-05 11:20:16 +0800

    onLoad onReady demo

---
+++ pages/demo6/demo6.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="layout">
+		页面生命周期
+		<scroll-view scroll-y="true" ref="scrollView">
+			<view>
+				11
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	import {onLoad,onReady} from '@dcloudio/uni-app'
+import { ref } from 'vue';
+	const scrollView = ref(null)
+	onLoad((a) => {
+		console.log("页面onLoad", a);
+	})
+	
+	onReady(() => {
+		console.log(scrollView.value);
+	})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
