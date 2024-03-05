f6d6e0fc2ebceb8ea57960ebf34399b969cddb5f
liuqi <723432427@qq.com>
2024-03-05 10:55:23 +0800

    defineExpose demo

--- components/Child/Child.vue
+++ components/Child/Child.vue
@@ -4,10 +4,13 @@
 			<slot :data="item"></slot>
 		</view>
 		<button type="default" @click="onClick">触发事件</button>
+		<image v-show="isShow" src="../../static/logo.png" mode="aspectFill"></image>
 	</view>
 </template>
 
 <script lang="ts" setup>
+	import { ref } from 'vue';
+
 	const emit = defineEmits<{
 		(event : 'add', id : number) : void
 	}>()
@@ -23,6 +26,20 @@
 	const onClick = () => {
 		emit('add', Math.random())
 	}
+
+	const isShow = ref(true)
+
+	const onShow = () => {
+		isShow.value = true
+	}
+
+	const onHide = () => {
+		isShow.value = false
+	}
+	
+	defineExpose({
+		onShow,onHide
+	})
 </script>
 
 <style lang="scss" scoped>
--- pages/demo5/demo5.vue
+++ pages/demo5/demo5.vue
@@ -1,26 +1,42 @@
 <template>
-	<Child @add = 'onAdd' :list = 'list'>
+	<Child @add='onAdd' :list='list' ref='child'>
 		<template #default="{data}">
 			<view>{{data.name}}</view>
 		</template>
 	</Child>
+	<button @click="showChild">显示子组件图片</button>
+	<button @click="hideChild">隐藏子组件图片</button>
 </template>
 
 <script lang="ts" setup>
-	import { ref } from 'vue'
-	const list = ref<{name: string}[]>([{
+	import { onMounted, ref } from 'vue'
+	const list = ref<{ name : string }[]>([{
 		name: '123'
-	}, 
+	},
 	{
 		name: '123'
 	}, {
 		name: '123'
 	}])
-const onAdd = (num: number) => {
-	list.value.push({
-		name: String(num)
+	const onAdd = (num : number) => {
+		list.value.push({
+			name: String(num)
+		})
+	}
+	
+	const child = ref(null)
+	onMounted(() => {
+		console.log(child.value);
 	})
-}
+	
+	const showChild = () => {
+		child.value.onShow()
+		
+	}
+	const hideChild = () => {
+		child.value.onHide()
+		
+	}
 </script>
 
 <style lang="scss" scoped>
